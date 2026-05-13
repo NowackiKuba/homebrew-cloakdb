caa1afe11fe7a0992fe38c5fe2375404afe1367e
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2026-05-13 15:38:01 +0200

    chore: fix

--- Formula/cloakdb.rb
+++ Formula/cloakdb.rb
@@ -5,16 +5,16 @@ class Cloakdb < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/NowackiKuba/cloakdb-api/releases/download/v0.1.0/cloakdb-darwin-arm64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-darwin-arm64.tar.gz"
       sha256 "PLACEHOLDER_ARM64"
     else
-      url "https://github.com/NowackiKuba/cloakdb-api/releases/download/v0.1.0/cloakdb-darwin-amd64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-darwin-amd64.tar.gz"
       sha256 "PLACEHOLDER_AMD64"
     end
   end
 
   on_linux do
-    url "https://github.com/NowackiKuba/cloakdb-api/releases/download/v0.1.0/cloakdb-linux-amd64.tar.gz"
+    url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-linux-amd64.tar.gz"
     sha256 "PLACEHOLDER_LINUX"
   end
 
