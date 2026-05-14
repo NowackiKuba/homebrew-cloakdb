f6f1afdf23c0f22305471aa34ec3b0620c2045b4
NowackiKuba <chujwdupechujowiichuj@gmail.com>
2026-05-14 16:29:18 +0200

    chore: fixes

--- Formula/cloakdb.rb
+++ Formula/cloakdb.rb
@@ -5,20 +5,20 @@ class Cloakdb < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-darwin-arm64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.1/cloakdb-darwin-arm64.tar.gz"
       sha256 "b204b2ee10bbba9fe28509b7dc59e25da374bc3394942bd81cda833447a4cfba"
     else
-      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-darwin-amd64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.1/cloakdb-darwin-amd64.tar.gz"
       sha256 "b4e08fef76693bf9aae9a28b21207728e933b27c85e6c97240ef743212fc0277"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-linux-arm64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.1/cloakdb-linux-arm64.tar.gz"
       sha256 "fc6b3d6f53c9a0c64f1b30817446c6fd8eea4350af4578aa43dea1efc077be0b"
     else
-      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.0/cloakdb-linux-amd64.tar.gz"
+      url "https://github.com/NowackiKuba/cloakdb-cli/releases/download/v0.1.1/cloakdb-linux-amd64.tar.gz"
       sha256 "1033fec78de4ec351a8aefebe9a31d541eecfd72aebed6557f738bbc9237d0bb"
     end
   end
